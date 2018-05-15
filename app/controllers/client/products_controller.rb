75e95429e27988789710b3a64be34f93faa5491b
saintlove <saintlovett@gmail.com>
2018-05-14 20:56:43 -0500

    added html routes

--- app/controllers/client/products_controller.rb
+++ app/controllers/client/products_controller.rb
@@ -4,4 +4,53 @@ class Client::ProductsController < ApplicationController
     @products = response.body
     render 'index.html.erb'
   end
+
+  def new
+    render 'new.html.erb'
+  end
+
+def create
+    client_params = {
+                         name: params[:name],
+                         price: params[:price],
+                         image_url: params[:image_url],
+                         description: params[:description],
+                         
+                    }
+    response = Unirest.post(
+                            "http://localhost:3000/api/products",
+                            parameters: client_params
+                            )
+    render 'create.html.erb'
+  end
+
+  def show
+    product_id = params[:id]
+    response = Unirest.get("http://localhost:3000/api/products/#{product_id}")
+    @product = response.body
+    render 'show.html.erb'
+  end
+  
+  def edit
+    product_id = params[:id]
+    response = Unirest.get("http://localhost:3000/api/products/#{product_id}")
+    @product = response.body
+    render 'edit.html.erb'
+  end
+
+  def update
+    client_params = {
+                         name: params[:name],
+                         price: params[:price],
+                         image_url: params[:image_url],
+                         description: params[:description],
+                         
+                         
+                         
+                    }
+    response = Unirest.patch(
+                             "http://localhost:3000/api/products/#{ params[:id] }",
+                             parameters: client_params
+                            )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get '/' => 'client/products#index'
   namespace :client do
    get '/products' => 'products#index'
+   get '/products/new' => 'products#new'
+   post '/products' => 'products#create'
+    get '/products/:id' => 'products#show'
+    get '/products/:id/edit' => 'products#edit'
+    patch '/products/:id' => 'products#update'
   end
 end
